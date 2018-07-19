a3444b3b443836d0b1a24737bbf5687809c31533
lw <me@lw.ag>
2018-07-19 11:32:11 +0200

    Replace GA dispatchers with stubs.

--- src/analytics.js
+++ src/analytics.js
@@ -1,4 +1,9 @@
 
-function analyticsEventReport(category, action, label, value){
-  chrome.runtime.sendMessage({gacategory: category, gaaction: action, galabel: label, gavalue: value});
+function analyticsEventReport(category, action, label, value) {
+    console.log(`supressed analytics report`);
+    console.log({ category, action, label, value });
 }
+
+// function analyticsEventReport(category, action, label, value){
+//   chrome.runtime.sendMessage({gacategory: category, gaaction: action, galabel: label, gavalue: value});
+// }
